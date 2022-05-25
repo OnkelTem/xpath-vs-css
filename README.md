9abe3c863ff5d6ba1b2745a6d0c33ef61b330e03
OnkelTem <aneganov@gmail.com>
2022-05-25 15:16:49 +0300

    Added average; added total

---
+++ README.md
@@ -0,0 +1,5 @@
+# DOM querying performance: XPath vs CSS selectors
+
+A very simple demo, showing the difference in performance of selecting DOM nodes between using [`document.querySelectorAll()`](https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll) and [`document.evaluate()`](https://developer.mozilla.org/en-US/docs/Web/API/Document/evaluate).
+
+Demo page: https://xpath-vs-css.vercel.app/
--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,30 @@ const SAMPLE_COUNT = 5000;
 declare const __DEBUG__: boolean;
 
 const template = `<section>
-  <ul>
-    <li>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</li>
-    <li>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</li>
-    <li>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</li>
-    <li>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</li>
-    <li>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</li>
-    <li>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</li>
-    <li>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</li>
-  </ul>
+  <div>
+    <p>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</p>
+    <p>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</p>
+    <p>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</p>
+    <p>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</p>
+    <p>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</p>
+    <p>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</p>
+    <p>Mauris placerat <strong>pellentesque nunc, <a href="#">in <span>pharetra</span> <span>diam</span></a> egestas ac.</strong> Integer at erat in dolor euismod sagittis. In hac habitasse platea dictumst.</p>
+  </div>
 </section>
 `;
 
-// window.requestAnimationFrame(() => {
-//   console.log('requestAnimationFrame');
-// });
+type Result = {
+  runs: number;
+  curr: number;
+  avg: number;
+};
+const results = new Map<string, Result>();
 
 if (document.readyState !== 'loading') {
   main();
 } else {
   document.addEventListener('DOMContentLoaded', main);
 }
-
 async function main() {
   dbg('Generating test dom');
   const message = document.getElementById('message')!;
@@ -40,10 +42,16 @@ async function main() {
     container.style.display = 'block';
     message.style.display = 'none';
 
-    const resCss = document.getElementById('resCss')!;
-    const resXpath = document.getElementById('resXpath')!;
-    const btnCss = document.getElementById('btnCss')!;
-    const btnXpath = document.getElementById('btnXpath')!;
+    const totalDiv = document.getElementById('total')!;
+
+    const cssContainer = document.getElementById('css')!;
+    const xpathContainer = document.getElementById('xpath')!;
+
+    const resCss = cssContainer.querySelector('.results')!;
+    const resXpath = xpathContainer.querySelector('.results')!;
+
+    const btnCss = cssContainer.querySelector('button')!;
+    const btnXpath = xpathContainer.querySelector('button')!;
 
     btnCss.removeAttribute('disabled');
     btnXpath.removeAttribute('disabled');
@@ -52,19 +60,61 @@ async function main() {
       btnCss.setAttribute('disabled', '');
       setTimeout(() => {
         const duration = cssSelectorBenchmark();
-        resCss.innerHTML = duration.toString();
+        saveResult('css', duration);
+        renderResults('css', resCss);
         btnCss.removeAttribute('disabled');
+        renderTotal(totalDiv);
       });
     });
     btnXpath.addEventListener('click', () => {
       btnXpath.setAttribute('disabled', '');
       setTimeout(() => {
         const duration = xpathSelectorBenchmark();
-        resXpath.innerHTML = duration.toString();
+        saveResult('xpath', duration);
+        renderResults('xpath', resXpath);
         btnXpath.removeAttribute('disabled');
+        renderTotal(totalDiv);
       });
     });
   }, 50);
+}
+
+function saveResult(benchmark: string, duration: number) {
+  if (results.has(benchmark)) {
+    const result = results.get(benchmark)!;
+    results.set(benchmark, {
+      curr: duration,
+      avg: (result.avg * result.runs + duration) / (result.runs + 1),
+      runs: result.runs + 1,
+    });
+  } else {
+    results.set(benchmark, {
+      curr: duration,
+      avg: duration,
+      runs: 1,
+    });
+  }
+}
+
+function renderResults(benchmark: string, el: Element) {
+  if (results.has(benchmark)) {
+    const result = results.get(benchmark)!;
+    el.querySelector('.runs')!.innerHTML = 'runs: ' + formatNumber(result.runs, 0);
+    el.querySelector('.current')!.innerHTML = 'curr: ' + formatNumber(result.curr) + 'ms';
+    el.querySelector('.average')!.innerHTML = 'avg: ' + formatNumber(result.avg) + 'ms';
+  }
+}
+
+function renderTotal(el: Element) {
+  if (results.has('css') && results.has('xpath')) {
+    const resultCss = results.get('css')!;
+    const resultXpath = results.get('xpath')!;
+    const [aStr, bStr, factor] =
+      resultCss.avg > resultXpath.avg
+        ? ['CSS-selector', 'XPath', formatNumber(resultCss.avg / resultXpath.avg)]
+        : ['XPath', 'CSS-selector', formatNumber(resultXpath.avg / resultCss.avg)];
+    el.innerHTML = `${aStr} is ${factor} times slower than ${bStr}`;
+  }
 }
 
 function cssSelectorBenchmark() {
@@ -121,6 +171,10 @@ if (window['$x'] == null) {
 
 function dbg(...params: any[]) {
   if (__DEBUG__) console.log('DBG: ', ...params);
+}
+
+function formatNumber(v: number, digits = 2) {
+  return '<tt>' + v.toFixed(digits) + '</tt>';
 }
 
 export {};
